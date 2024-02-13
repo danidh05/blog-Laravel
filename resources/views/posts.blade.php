bec8d5fc08dc049a6a584fe257f9fe7518402724
danidh05 <150284540+danidh05@users.noreply.github.com>
2024-02-13 13:42:34 +0200

    intro to blade templates

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -11,12 +11,13 @@
 @foreach($posts as $post)
         <article>
            <h1>
-             <a href="/posts/<?=$post->slug;?>">
-            <?=$post->title;?>
+             <a href="/posts/{{ $post->slug }}">
+           
+            {{ $post->title}}
             </a> 
            </h1>
            <div>
-            <?=$post->body;?>
+            {!!$post->body!!}
            </div>
         </article>
     @endforeach
